7d68f85538e06a9455a17ea14b6cccdae0a0bd93
skulis <skulis@4525493e-7705-40b1-a816-d608a930855b>
2013-08-08 15:01:23 +0000

    setup_producer script improved

--- setup_producer.sh
+++ setup_producer.sh
@@ -1,4 +1,4 @@
- export TPPROD=/home/lcd/CLIC_Testbeam_August2013/eudaq/TimepixProducer
  export EUDAQ=/home/lcd/CLIC_Testbeam_August2013/eudaq
+ export TPPROD=$EUDAQ/TimepixProducer
  export LD_LIBRARY_PATH=$EUDAQ/bin:$LD_LIBRARY_PATH 
  export LD_LIBRARY_PATH=$EUDAQ/TimepixProducer/Pixelman_SCL_2011_12_07:$LD_LIBRARY_PATH
