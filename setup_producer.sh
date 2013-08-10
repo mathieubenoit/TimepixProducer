37a5fb10a9c95dacd8973e433601f741d13477f1
mbenoit <mbenoit@4525493e-7705-40b1-a816-d608a930855b>
2013-08-10 06:34:07 +0000

    added root

--- setup_producer.sh
+++ setup_producer.sh
@@ -1,4 +1,5 @@
- export TP_CONFIG=/home/lcd/CLIC_Testbeam_August2013/TimepixAssemblies_Data
+ source /home/lcd/root/bin/thisroot.sh
+ export TPPROD=/home/lcd/CLIC_Testbeam_August2013/eudaq/TimepixProducer
  export EUDAQ=/home/lcd/CLIC_Testbeam_August2013/eudaq
  export TPPROD=$EUDAQ/TimepixProducer
  export LD_LIBRARY_PATH=$EUDAQ/bin:$LD_LIBRARY_PATH 
